2a406e7964669f5adc9ca5184f3cec3606a42865
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-16 16:21:53 +0100

    test: Test function find genre by API

--- src/components/FilterGenreBox.vue
+++ src/components/FilterGenreBox.vue
@@ -6,12 +6,14 @@
       id="selectGenre"
       @change="$emit('filter-genre', genreSelected)"
     >
+      <option value="">test</option>
       <option value="">All</option>
       <option value="Rock">Rock</option>
       <option value="Pop">Pop</option>
       <option value="Jazz">Jazz</option>
       <option value="Metal">Metal</option>
     </select>
+    <button class="p-5" @click="generi">TEST</button>
   </div>
 </template>
 
@@ -20,7 +22,28 @@ export default {
   data() {
     return {
       genreSelected: "",
+      allGenre: [],
     };
+  },
+
+  props: {
+    dischi: Array,
+  },
+
+  methods: {
+    generi() {
+      // console.log(this.dischi);
+      const allGenre = [];
+
+      this.dischi.forEach((disco) => {
+        //console.log(disco.genre);
+        if (!allGenre.includes(disco.genre)) {
+          allGenre.push(disco.genre);
+        }
+      });
+      console.log(allGenre);
+      return allGenre;
+    },
   },
 };
 </script>
